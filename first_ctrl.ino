f6802a0f2f35bbd7e78d2c771623ebb13f79353a
Sakamoto <yskibouyskibou@gmail.com>
2015-02-21 01:50:13 +0900

    add Serialsetting

--- first_ctrl.ino
+++ first_ctrl.ino
@@ -1,12 +1,17 @@
 void setup()
 {
+  
   pinMode( 8,OUTPUT);
   pinMode( 9,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop()
 {
+  
+  Serial.println("aaa");
   delay(1000);
 }
